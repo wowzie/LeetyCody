2b20342f2ae2bb666eccf31a6c31bc18f0670dee
Tushar Mohan <mohan.tushar16@gmail.com>
2024-06-27 15:32:48 +0100

    update

---
+++ Lc121.java
@@ -0,0 +1,10 @@
+public class Lc121 {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        return 0;
+    }
+}
